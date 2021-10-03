2775df10d4ad1cd5ae4f87831341eeae15a91f2b
Yaroslav Magrelo <slava.magrelo@gmail.com>
2021-10-03 16:40:31 +0300

    added 5.1_clousersAtPractice

--- 5_clousers/5.1_clousers.js
+++ 5_clousers/5.1_clousers.js
@@ -1,17 +1,33 @@
-import {assert} from "../helpers.js";
+// import {assert} from "../helpers.js";
+//
+// let outerValue = 'samurai';
+// let later;
+//
+// function outerFunction() {
+//   let innerValue = 'ninja';
+//   function innerFunction() {
+//     assert(outerValue === 'samurai', 'samurai, I can see the samurai');
+//     assert(innerValue === 'ninja', 'ninja, I can see the ninja')
+//   }
+//
+//   later = innerFunction;
+// }
+//
+// outerFunction();
+// later();
 
-let outerValue = 'samurai';
-let later;
 
-function outerFunction() {
-  let innerValue = 'ninja';
-  function innerFunction() {
-    assert(outerValue === 'samurai', 'samurai, I can see the samurai');
-    assert(innerValue === 'ninja', 'ninja, I can see the ninja')
-  }
-
-  later = innerFunction;
+function matrix(array) {
+  return console.log(array.map((innerArr) => console.log(innerArr)));
 }
 
-outerFunction();
-later();
+let arr = [
+    [-1,  4, -5, -9,  3 ],
+    [ 6, -4, -7,  4, -5 ],
+    [ 3,  5,  0, -9, -1 ],
+    [ 1,  5, -7, -8, -9 ],
+    [-3,  2,  1, -5,  6 ]
+];
+
+
+matrix(arr);
---
+++ 5_clousers/5.2_clousersAtPractice.js
@@ -0,0 +1,25 @@
+import {assert} from "../helpers.js";
+
+//from creating private variable
+
+function Ninja() {
+  let feints = 0;
+  this.getFeints = function () {
+    return feints;
+  }
+
+  this.feint = function () {
+    feints++;
+  }
+}
+
+const ninja1 = new Ninja();
+ninja1.feint();
+
+
+assert(ninja1.feints === undefined, 'And the private data is inaccessible to us');
+assert(ninja1.getFeints() === 1, 'We are able to access the internal feint count');
+
+const ninja2 = new Ninja();
+
+assert(ninja2.getFeints() === 0, 'The second ninja object gets its own feints variable');
